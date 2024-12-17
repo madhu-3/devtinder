2b03507103c5653e8127475f4eb94a61bbca9495
Madhu Yelubandy <madhuyelubandi@gmail.com>
2024-12-17 17:58:06 +0530

    route_practice

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Ep-3
   understand node_modules,package.json,package-lock.json, dependencies, dev dependencies, -g, Caret and tilde(^ vs ~)
   Install express, create server,listen to port,write req handlers for different routes
   Install Nodemon, update the scripts in package.json
+Ep-4
+  Express Routing
+  Explore use of ? * + () in routes
+  use of regex in routes
+  reading query params and dynamic routes 
--- src/app.js
+++ src/app.js
@@ -14,14 +14,42 @@ const app = express();
 // app.use('/user',(req,res)=>{
 //   res.send('Sending')
 // })
+// app.get("/user",(req,res)=>{
+//   res.send({firstName:'Madhu',lastName:'Yelubandy'})
+// })
+// app.post("/user",(req,res)=>{
+//   res.send('updated successfully')
+// })
+// app.delete("/user",(req,res)=>{
+//   res.send('deleted')
+// })
+
+app.get('/a+bc',(req,res)=>{
+  res.send('hey working??')
+})
+
+app.get('/ab*cd',(req,res)=>{
+  res.send('from star *')
+})
+
+//y is optional
+app.get('/xy?cd',(req,res)=>{
+  res.send('from question?')
+})
+
+app.get('/mn(ab)*op',(req,res)=>{
+  res.send('from ()')
+})
+
 app.get("/user",(req,res)=>{
-  res.send({firstName:'Madhu',lastName:'Yelubandy'})
+  console.log(req.query)
+  res.send('got successfully')
+
 })
-app.post("/user",(req,res)=>{
-  res.send('updated successfully')
-})
-app.delete("/user",(req,res)=>{
-  res.send('deleted')
+app.post("/user/:userId/:userName/:password",(req,res)=>{
+  console.log(req.params)
+  res.send('Saved successfully')
+
 })
 app.listen(3000, () => {
   console.log("Successfully running on port 3000");
